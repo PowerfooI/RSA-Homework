68ac1670484d2cd88f2a03159514f11b94031464
wangshuyi <sy_wang16@163.com>
2020-11-07 21:41:37 +0800

    fix last commit

--- golangv/main.go
+++ golangv/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/big"
+)
+func QuickExpMod(base *big.Int, exponent *big.Int, modulo *big.Int) *big.Int {
+	res := big.NewInt(1)
+	for exponent.Cmp(big.NewInt(1))>0 {
+		q, m := exponent.DivMod(exponent, big.NewInt(2), nil)
+		if m.Cmp(big.NewInt(1)) == 0 {
+			res.Mul(res, base.Mod(base, modulo))
+		}
+		base.Mul(base, base.Mod(base, modulo))
+		exponent = q
+	}
+	return res
+}
 
+func ExtEuclid(a *big.Int, b *big.Int) (*big.Int, *big.Int, *big.Int) {
+	if b.Cmp(big.NewInt(0)) == 0{
+		return big.NewInt(1), big.NewInt(0), a
+	} else {
+		u, v, q := ExtEuclid(b, a.Mod(a, b))
+		u, v = v, u.Sub(u, a.Div(a, b).Mul(a, v))
+		return u, v, q
+	}
+}
+
+func main() {
+	fmt.Println(ExtEuclid(big.NewInt(24), big.NewInt(16)))
 }
